f35e2e9b9f084f674944b0b033df9b8d5760c97f
YerayIBF <yerayalbesafp@ibf.cat>
2024-11-12 13:58:37 +0100

    seccion 1 home hecha + header hecho

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
         <div class="headerSup">
@@ -15,22 +15,20 @@
 
     <div class="logo-menu">
       <div class="logo">
-        <img>
+        <img src="img/Logo.png">
       </div>
     </div>
 
-  
     <nav class="menu">
       <a href="#">INICIO</a>
       <a href="#">CARTA</a>
     </nav>
-
   
     <div class="seccion-iconos">
       <input type="text" class="buscador" placeholder="Buscar">
-      <a class="icono"><img></a>
-      <a class="icono"><img></a>
-      <a class="icono"><img></a>
+      <a class="icono"><img src="img/iconoLupa.png"></a>
+      <a class="icono"><img src="img/iconoUsuario.png"></a>
+      <a class="icono"><img src="img/iconoCarrito.png"></a>
       <p class="numcarrito">0</p> 
     </div>
   </div>
--- view/home.php
+++ view/home.php
@@ -1,29 +1,82 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AprilPlants</title>
-    <link rel="stylesheet" href="css\home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
     
     <main>
         <div class="banner">
-            <h2>LLENA TU MESA DE SABOR Y SALUD</h2>
-            <h1>Los platos que<br> nutren tu cuerpo y alma</h1>
-            <button>VER CARTA</button>
+            
+            <div class="contenedor-banner">
+                <h2>LLENA TU MESA DE SABOR Y SALUD</h2>
+                <h1>Los platos que<br> <i>nutren tu cuerpo y alma</i></h1>
+                <button>VER CARTA</button>
+            </div>
         </div>
         
-        <div>
+        <div class="seccion1-titulo">
             <h2>Platos</h2>
             <p>Los mas vendidos</p>
         </div>
 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="plato-card text-center">
+                        <img src="img/mezclaTempura.png" class="plato-img" alt="Mezcla de Tempura">
+                        <button class="btn-anadir">AÑADIR AL CARRITO</button>
+                    </div>
+                    <div class="text-center">
+                        <h5>Mezcla De Tempura</h5>
+                        <p>DESDE €12,99</p>
+                    </div>
+                </div>
+            <div class="col-md-3">
+                <div class="plato-card text-center">
+                    <img src="img/albahacatailandesa.png" class="plato-img" alt="Albahaca Tailandesa">
+                    <button class="btn-anadir">AÑADIR AL CARRITO</button>
+                </div>
+                <div class="text-center">
+                    <h5>Albahaca tailandesa</h5>
+                    <p>DESDE €18</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="plato-card text-center">
+                    <img src="img/Tortitas.webp" class="plato-img" alt="Tortitas">
+                    <button class="btn-anadir">AÑADIR AL CARRITO</button>
+                </div>
+                <div class="text-center">
+                    <h5>Tortitas</h5>
+                    <p>DESDE €11</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="plato-card text-center">
+                    <img src="img/pastelDeArroz.png" class="plato-img" alt="Pastel De Arroz">
+                    <button class="btn-anadir">AÑADIR AL CARRITO</button>
+                </div>
+                <div class="text-center">
+                    <h5>Pastel De Arroz</h5>
+                    <p>DESDE €14</p>
+                </div>
+            </div>
+        </div>
 
-        
+        <div class="text-center my-5">
+            <button class="btn-ver-platos">VER PLATOS</button>
+        </div>
+    </div>
+
+
+
     </main>
-    
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
