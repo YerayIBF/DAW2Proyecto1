3433b69dbf53bf794a339f4db432f0f58530daba
Yeray Albesa <yerayalbesafp@ibf.cat>
2024-11-20 20:17:42 +0100

    Acabado el inicio de session, creacion del archivo js para el panel compra, pequeños cambios en home.css y footer, añadido otra imagen de mayor calidad para el banner, casi terminado cuentausuario.php

--- model/usuario.php
+++ model/usuario.php
@@ -1,98 +1,20 @@
 <?php
 class Usuario {
-    protected $ID_Usuario;
-    protected $nombre;
-    protected $correo;
-    protected $contraseña;
-    protected $rol;
+    protected $ID_Usuario; 
+    
+    protected $Nombre; 
+    
+    protected $Correo; 
+    
+    protected $Contraseña;
+    
+    protected $Rol;
+    
 
-    public function __construct($ID_Usuario = null, $nombre  = null, $correo  = null, $contraseña  = null, $rol = 'usuario') {
-        $this->ID_Usuario = $ID_Usuario;
-        $this->nombre = $nombre;
-        $this->correo = $correo;
-        $this->contraseña = $contraseña;
-        $this->rol = $rol;
+    public function __construct() {
+
     }  
-
-    /**
-     * Get the value of nombre
-     */ 
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    /**
-     * Set the value of nombre
-     *
-     * @return  self
-     */ 
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of correo
-     */ 
-    public function getCorreo()
-    {
-        return $this->correo;
-    }
-
-    /**
-     * Set the value of correo
-     *
-     * @return  self
-     */ 
-    public function setCorreo($correo)
-    {
-        $this->correo = $correo;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of contraseña
-     */ 
-    public function getContraseña()
-    {
-        return $this->contraseña;
-    }
-
-    /**
-     * Set the value of contraseña
-     *
-     * @return  self
-     */ 
-    public function setContraseña($contraseña)
-    {
-        $this->contraseña = $contraseña;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of rol
-     */ 
-    public function getRol()
-    {
-        return $this->rol;
-    }
-
-    /**
-     * Set the value of rol
-     *
-     * @return  self
-     */ 
-    public function setRol($rol)
-    {
-        $this->rol = $rol;
-
-        return $this;
-    }
+    
 
     /**
      * Get the value of ID_Usuario
@@ -113,5 +35,84 @@ class Usuario {
 
         return $this;
     }
+
+    /**
+     * Get the value of Nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    /**
+     * Set the value of Nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($Nombre)
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Correo
+     */ 
+    public function getCorreo()
+    {
+        return $this->Correo;
+    }
+
+    /**
+     * Set the value of Correo
+     *
+     * @return  self
+     */ 
+    public function setCorreo($Correo)
+    {
+        $this->Correo = $Correo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Contraseña
+     */ 
+    public function getContraseña()
+    {
+        return $this->Contraseña;
+    }
+
+    /**
+     * Set the value of Contraseña
+     *
+     * @return  self
+     */ 
+    public function setContraseña($Contraseña)
+    {
+        $this->Contraseña = $Contraseña;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Rol
+     */ 
+    public function getRol()
+    {
+        return $this->Rol;
+    }
+
+    /**
+     * Set the value of Rol
+     *
+     * @return  self
+     */ 
+    public function setRol($Rol)
+    {
+        $this->Rol = $Rol;
+
+        return $this;
+    }
 }
-?>
--- view/cuentausuario.php
+++ view/cuentausuario.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuenta Usuario April</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/cuentausuario.css">
 </head>
+
 <body>
     <main>
-        <H1>Mi cuenta</H1>
-        <a href="?controller=producto&action=cerrarSession">Cerrar sesión</a>
+        <section class="container-fluid">
+            <div class="text-center mb-4">
+                <h1 class="h1-cuenta">Mi cuenta</h1>
+                <a href="?controller=producto&action=cerrarSession" class="a-cuenta">Cerrar sesión</a>
+            </div>
+            <div class="row">
 
-        
+                <div class="col-md-8  ">
+                    <h2 class="h2-cuenta margen-h2-cuenta">HISTORIAL DE PEDIDOS</h4>
+                    <p>No ha realizado algún pedido aún.</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h2 class="h2-cuenta margen-h2-cuenta">DETALLES DE LA CUENTA</h4>
+                    <a href="#" class="boton-cuenta">VER DIRECCIONES (0)</a>
+                </div>
+            </div>
+        </section>
     </main>
 </body>
+
 </html>
