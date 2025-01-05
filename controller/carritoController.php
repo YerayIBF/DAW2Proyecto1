e594b5b8bddc94b14921968517206855161e965c
YerayIBF <yerayalbesafp@ibf.cat>
2025-01-05 08:11:53 +0100

    Preparacion de commit para ordenar el controlador en varios

---
+++ controller/carritoController.php
@@ -0,0 +1,16 @@
+<?php
+include_once("model/PlatosDAO.php");
+include_once("model/Platos.php");
+include_once("model/usuario.php");
+include_once("model/usuarioDAO.php");
+include_once("model/pedido.php");
+include_once("model/PedidoDAO.php");
+include_once("model/oferta.php");
+include_once("model/ofertaDAO.php");
+include_once("model/detallePedido.php");
+include_once("model/detallePedidoDAO.php");
+
+class carritoController{
+
+}
+
